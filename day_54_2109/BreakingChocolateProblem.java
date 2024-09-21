25ca4b8a5684ca1835bf34ec9768181e85b5a815
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2024-09-21 20:31:25 +0300

    Create BreakingChocolateProblem.java

---
+++ day_54_2109/BreakingChocolateProblem.java
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/534ea96ebb17181947000ada/train/csharp
+
+public class Kata 
+{
+  public static int BreakChocolate(int n, int m) {
+    if (n < 1 || m < 1) {
+        return 0;
+    }
+    return (n * m - 1);
+  }
+}
