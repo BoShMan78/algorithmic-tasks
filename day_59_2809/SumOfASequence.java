1ff5080125167cff5fceeedb082e6d2b04513acd
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2024-09-28 14:15:21 +0300

    Create SumOfASequence.java

---
+++ day_59_2809/SumOfASequence.java
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/586f6741c66d18c22800010a/train/java
+
+public class Kata {
+  public static int sequenceSum(int start, int end, int step) {
+    if (end < start) {
+            return 0;
+    }
+    int sum = 0;
+    for (int i = start; i <= end ; i += step) {
+            sum += i;
+    }
+    return sum;
+  }
+}
