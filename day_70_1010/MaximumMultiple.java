143d835fa6c7c2c8620268c01d22b9ba90307a6f
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2024-10-10 16:27:46 +0300

    Create MaximumMultiple.java

---
+++ day_70_1010/MaximumMultiple.java
@@ -0,0 +1,12 @@
+// https://www.codewars.com/kata/5aba780a6a176b029800041c/train/java
+
+public class MaxMultiple {
+  public static int maxMultiple(int divisor, int bound) {
+    for (int i = bound; i >= divisor; i--) {
+            if (i % divisor == 0) {
+                return i;
+            }
+      }
+      return 0;
+  }
+}
