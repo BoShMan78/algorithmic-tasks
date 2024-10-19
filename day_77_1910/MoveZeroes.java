9c7ed1bc7fd62fd39d0519af1b0368a8816b208b
Bohdan Shevtsov <114682516+BoShMan78@users.noreply.github.com>
2024-10-19 17:55:05 +0300

    Create MoveZeroes.java

---
+++ day_77_1910/MoveZeroes.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/move-zeroes/submissions/1427377615/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - 1; j++) {
+                if (nums[j] == 0) {
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = 0;
+                }
+            }
+        }
+    }
+}
